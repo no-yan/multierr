64e5e1968d38ae3d451f85628d33a3d7c65772d8
no-yan <63000297+no-yan@users.noreply.github.com>
2025-01-15 00:55:39 +0900

    doc: write godoc

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
+// package multierr provides a simple way to collect errors
+// and return them as a single error using errors.Join.
 package multierr
 
 import "errors"
 
-type Builder interface {
+// Collector collects multiple errors and returns them as a single error.
+// Errors can be added by [multierr.Collector.Add], and the final combined error is obtained by [multierr.Collector.Err].
+//
+// The resulting error supports [errors.Is] and [errors.As] checks
+// for any of the collected errors.
+type Collector interface {
+	// Add appends an error to the collection.
 	Add(error)
+	// Err returns all collected errors as a single error, by calling errors.Join.
+	// If no errors have been added, Err returns nil.
 	Err() error
 }
 
-type builder struct {
+type collector struct {
 	errs []error
 }
 
-func (b *builder) Add(err error) {
-	b.errs = append(b.errs, err)
+func (c *collector) Add(err error) {
+	c.errs = append(c.errs, err)
 }
 
-func (b *builder) Err() error {
-	return errors.Join(b.errs...)
+func (c *collector) Err() error {
+	return errors.Join(c.errs...)
 }
 
-func New() Builder {
-	return &builder{
+// New creates a new Collector.
+//
+// This function is not concurrency-safe: if you plan to use the collector
+// from multiple goroutines, consider wrapping calls with a mutex or
+// using your own concurrency control.
+func New() Collector {
+	return &collector{
 		errs: make([]error, 0),
 	}
 }
